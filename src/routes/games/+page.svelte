ba1f056b42c1847bce37f61bef2c074866c82324
Ben Tomlin <bentomlin101@gmail.com>
2023-02-07 00:35:21 -0800

    finished play page and stats page mostly

---
+++ src/routes/games/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import LabeledNumber from "$lib/LabeledNumber.svelte";
+import { games, type Game } from "../stats/stats";
+</script>
+
+{#each $games as { score, hints, mistakes }}
+    <div class="flex">
+        <LabeledNumber 
+    </div>
+{/each}
