6cad00a4173c2e1fd9a14fa8749c3baa855a4be8
Ben Tomlin <bentomlin101@gmail.com>
2023-02-08 18:42:12 -0800

    added titles

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
 	import { page } from "$app/stores";
-	const nav = [
-		{ url: "/", name: "Play" },
-		{ url: "/games", name: "Games" },
-		{ url: "/stats", name: "Stats" }
-	];
+	let nav = new Map([
+		["/", "Play"],
+		["/games", "Games"],
+		["/stats", "Stats"]
+	]);
 </script>
+
+<svelte:head>
+	<title>{nav.get($page.url.pathname) ?? ""} | Pi Game</title>
+</svelte:head>
 
 <div class="flex items-center justify-center">
 	<nav
 		class="flex h-16 w-fit items-center justify-center rounded-full bg-fuchsia-800 px-5 text-xl font-bold uppercase text-[#14001a] decoration-4 underline-offset-4"
 	>
-		{#each nav as { url, name }}
+		{#each Array.from(nav.entries()) as [url, name]}
 			<span class:underline={$page.url.pathname === url}>
 				<a href={url} class="px-10 py-3">{name}</a>
 			</span>
